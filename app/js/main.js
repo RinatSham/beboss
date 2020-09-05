55f2dff94cd2f8fc7efed8120b2772b0e16d6597
RinatSham <adaptor2012@gmail.com>
2020-09-05 11:55:59 +0300

    check

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+
+   $('.partner__slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      responsive: [
+         {
+           breakpoint: 1024,
+           settings: {
+             slidesToShow: 2,
+           }
+         },
+         {
+            breakpoint: 630,
+            settings: {
+              slidesToShow: 1,
+            }
+         }
+      ]
+    });
+
+
+    $('input[type="checkbox"]').styler();
+
+});
